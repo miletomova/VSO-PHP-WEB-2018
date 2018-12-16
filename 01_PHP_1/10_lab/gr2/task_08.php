e58ec6dd67f848523cf1c4ab3672e5c3bbae4307
Milena Tomova <kokolina1888@gmail.com>
2018-12-16 16:43:26 +0200

    sinc

--- 01_PHP_1/10_lab/gr2/task_08.php
+++ 01_PHP_1/10_lab/gr2/task_08.php
@@ -1,13 +1,16 @@
 <?php 
 
-$arr = [1, 8, -9, 0];
+$arr = [-10, 8, -90, 0];
 
 $min = $arr[0];
+$min_ind = 0;
 
 for($i = 1; $i < count($arr); $i++){
 	if($min > $arr[$i]){
 		$min = $arr[$i];
+		$min_ind = $i;
 	}
 }
 
 echo $min;
+echo $min_ind;
