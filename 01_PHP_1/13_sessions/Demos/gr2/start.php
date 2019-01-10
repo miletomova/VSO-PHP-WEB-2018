4d148804f2116029ac907ca4b0fd53784b52992b
Milena Tomova <kokolina1888@gmail.com>
2019-01-10 19:48:46 +0200

    sinc

---
+++ 01_PHP_1/13_sessions/Demos/gr2/start.php
@@ -0,0 +1,25 @@
+<?php 
+	session_start();
+
+	echo session_id();
+	$product1 = 'Orange';
+	$product2 = 'Apple';
+	$_SESSION['p1'] = $product1;
+	$_SESSION['product2'] = $product2;
+
+	$product1 = 'banana';
+	$_SESSION['p1'] = $product1;
+	
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Welcome - <?= date('H:i:s') ?></h1>
+
+	<a href="second.php">Get Session Info</a>
+</body>
+</html>
